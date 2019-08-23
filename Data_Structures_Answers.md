fb2d27c1480384034ed1778f849a95535635abc7
Timothy Shores <timothymshores@gmail.com>
2019-08-23 12:38:34 -0500

    ring_buffer.py passes testing_ring_buffer.py

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -10,8 +10,11 @@ Add your answers to the questions below.
 
 
 5. What is the runtime complexity of the provided code in `names.py`?
+O(n^2) due to nested for loops
 
 6. What is the space complexity of the provided code in `names.py`?
+names_1 is a list with n elements where n is the number of lines in names_1.txt
+names_2 is a list with n elements where n is the number of lines in names_2.txt
 
 7. What is the runtime complexity of your optimized code in `names.py`?
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,50 +2,25 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = 0
-        self.storage = []
+        self.storage = [None]*capacity
 
     def append(self, item):
-        # storage is at capacity and first item is the oldest
-        if len(self.storage) == self.capacity and self.current == len(self.storage):
-            # remove first element and insert value into index 0
-            self.storage.pop(0)
-            self.storage.insert(0, item)
+        # self.current pointer is at end of self.storage
+        if self.current == self.capacity:
+            # assign item to the first element of self.storage
+            self.storage[0] = item
+            # set self.current pointer to index 1
             self.current = 1
-        # storage is at capacity and second item is the oldest
-        elif:
-            pass
-        # storage is not at capacity, append item and increment current
         else:
-            self.storage.append(item)
+            # assign item to self.current pointer index of self.storage
+            self.storage[self.current] = item
+            # increment self.current pointer
             self.current += 1
 
+    # return self.storage with all occurences of None removed
     def get(self):
+        return list(filter(lambda a: a != None, self.storage))
+
+    # helper debugging method
+    def debug(self):
         return f"Storage: {self.storage}, Current: {self.current}"
-
-
-buffer = RingBuffer(3)
-
-print("should return []")   # should return []
-print(buffer.get())   # should return []
-
-buffer.append('a')
-print(buffer.get())   # should return ['a'], 1
-buffer.append('b')
-print(buffer.get())   # should return ['a', 'b'], 2
-buffer.append('c')
-print(buffer.get())   # should return ['a', 'b', 'c'], 3
-
-
-print("should return ['a', 'b', 'c']")
-print(buffer.get())   # should return ['a', 'b', 'c']
-
-# 'd' overwrites the oldest value in the ring buffer, which is 'a'
-buffer.append('d')
-
-print("should return ['d', 'b', 'c']")
-print(buffer.get())   # should return ['d', 'b', 'c']
-
-buffer.append('e')
-buffer.append('f')
-
-buffer.get()   # should return ['d', 'e', 'f']
